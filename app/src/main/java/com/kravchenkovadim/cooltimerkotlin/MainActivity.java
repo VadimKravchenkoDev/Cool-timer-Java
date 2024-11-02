3386cd6ffbffc1d318548290719546a194430df5
Arhizmei <arhizmei@gmail.com>
2024-11-02 20:37:42 +0200

    add timer

--- app/src/main/java/com/kravchenkovadim/cooltimerkotlin/MainActivity.java
+++ app/src/main/java/com/kravchenkovadim/cooltimerkotlin/MainActivity.java
@@ -1,9 +1,10 @@
 package com.kravchenkovadim.cooltimerkotlin;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +14,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    Boolean check = true;
+    Boolean checkTimer = true;
+    Button button;
+    CountDownTimer timer;
+    TextView textView;
+    private long secondLeft = 60000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +32,37 @@ public class MainActivity extends AppCompatActivity {
         });
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
     }
 
     public void onClick(View view) {
-        Button button = findViewById(R.id.button);
-        if (check) {
-            button.setText("STOP");
-            check = false;
+        if (checkTimer) {
+            timer = new CountDownTimer(secondLeft, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                textView.setText("00:" + String.valueOf(millisUntilFinished / 1000));
+                secondLeft = millisUntilFinished;
+            }
+
+            @Override
+            public void onFinish() {
+            }
+        };
+            startTimer();
         } else {
-            button.setText("START");
-            check = true;
+            stopTimer();
         }
+    }
+    public void startTimer(){
+        timer.start();
+        button.setText("PAUSE");
+        checkTimer = false;
+    }
+    public void stopTimer(){
+        button.setText("START");
+        checkTimer = true;
+        timer.cancel();
     }
 }
